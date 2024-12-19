6da810ea050e05f9c827b15f44b129dad733d9dc
Farooq Cheema <farooq.orionid@gmail.com>
2024-12-19 18:33:34 +0500

    final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Header from "./components/Header";
-import LandingSection from "./components/LandingSection";
-import ProjectsSection from "./components/ProjectsSection";
-import ContactMeSection from "./components/ContactMeSection";
-import Footer from "./components/Footer";
-import { AlertProvider } from "./context/alertContext";
-import Alert from "./components/Alert";
+import './App.css';
+import { useState, useEffect, React } from 'react';
+import { Navbar, Header, Main, Footer, Reservations, OrderOnline } from './components';
+import {Routes, Route, useLocation  } from "react-router-dom";
+import { headerData, reservationData, orderOnline } from './constants';
+import DataContext from './DataContext';
+import ConfirmedBooking from './components/Reservations/ConfirmedBooking';
 
 function App() {
+// use state to set data
+// use effect to get path
+  const location = useLocation();
+  const [path, setPath] = useState(location.pathname);
+
+  // set path name on route change
+  useEffect(()=> {
+    setPath(location.pathname);
+  }, [location.pathname]);
+
+
+    // Send data to header to based on route
+    let data = {};
+    if(path === '/' || path === '/Little-Lemon') {
+
+      data = headerData;
+
+    } else if (path === '/Reservations') {
+
+      data = reservationData;
+
+    } else if (path === '/OrderOnline') {
+
+      data = orderOnline;
+
+    }
+
+    
   return (
-    <ChakraProvider>
-      <AlertProvider>
-        <main>
-          <Header />
-          <LandingSection />
-          <ProjectsSection />
-          <ContactMeSection />
+      <>
+      
+          <Navbar />
+            <DataContext.Provider value={data}>
+                <Routes>
+                  <Route path='/Little-Lemon' element ={ 
+                    <>
+
+                      <Header />
+                      <Main />
+
+
+                    </>
+                  } />                  
+                  <Route path='/' element ={ 
+                    <>
+
+                      <Header />
+                      <Main />
+
+
+                    </>
+                  } />
+                  <Route path='/Reservations' element={
+                    <>
+
+                      <Header />
+                      <Reservations />
+
+                        
+                    </>
+                  } />
+                  <Route path='/OrderOnline' element={
+                    <>
+                      <Header />
+                      <OrderOnline />  
+                    </>
+                  } />                  
+
+                  {/* Path for booking confirmation */}
+                  <Route path='/ConfirmedBooking' element={<ConfirmedBooking/>}/>
+
+                </Routes>
+            </DataContext.Provider>
           <Footer />
-          <Alert />
-        </main>
-      </AlertProvider>
-    </ChakraProvider>
+
+      </>
   );
 }
 
